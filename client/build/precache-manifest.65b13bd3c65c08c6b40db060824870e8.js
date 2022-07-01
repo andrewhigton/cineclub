30e32ada003f7a2b496f86407f86f59d7390c3e3
andrewhigton <andrewhigton@outlook.com>
2022-07-01 21:49:22 +0100

    runbuild

---
+++ client/build/precache-manifest.65b13bd3c65c08c6b40db060824870e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8d1b8ade01ce0cff150848d5b0e597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0dfebc3bb7394487aab",
+    "url": "/static/css/2.1ebb0010.chunk.css"
+  },
+  {
+    "revision": "41c65e1a87689770ff3c",
+    "url": "/static/css/main.e4e04e13.chunk.css"
+  },
+  {
+    "revision": "b0dfebc3bb7394487aab",
+    "url": "/static/js/2.5126eb05.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.5126eb05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c65e1a87689770ff3c",
+    "url": "/static/js/main.73603c84.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.73603c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
